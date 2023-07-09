4f464ef7e8c7b187aee2201a603cf39a1db8c37b
valb-mig <valb.mig@gmail.com>
2023-07-09 16:34:38 -0300

    [Api conn login]

--- public/index.php
+++ public/index.php
@@ -5,6 +5,13 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    exit;
+}
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 
-Route::post('/login',    [userController::class, 'login']); 
+Route::post('/login',    [userController::class, 'login']);
 Route::post('/register', [userController::class, 'register']); 
 
 Route::get('/', function(){
